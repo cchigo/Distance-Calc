9c11b035657db89b335c719c543d9435c20113b0
cchigo <ccasadu15@gmail.com>
2020-01-15 14:38:57 +0100

    create Location viewmodel

---
+++ app/src/main/java/com/chigo/distancecalc/LocationViewModel.kt
@@ -0,0 +1,36 @@
+package com.chigo.distancecalc
+
+import android.content.Context
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.chigo.distancecalc.data.Location
+import com.chigo.distancecalc.data.LocationDatabase
+
+class LocationViewModel: ViewModel() {
+
+    private val location: MutableLiveData<List<Location>> by lazy {
+        MutableLiveData<List<Location>>().also{
+            loadAllLocation()
+        }
+    }
+
+    private fun loadAllLocation(){
+
+    }
+
+    fun getLocation(context: Context): LiveData<List<Location>> {
+        return LocationDatabase.getInstance(context).LocationDao().getLocationInfo()
+    }
+
+    fun setLocation(location: Location, context: Context){
+        return LocationDatabase.getInstance(context).LocationDao().insert(location)
+    }
+
+    fun updateLocation(location: Location, context: Context){
+        return LocationDatabase.getInstance(context).LocationDao().update(location)
+    }
+
+
+
+}
