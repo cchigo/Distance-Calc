9909a32a136f02f24be11efa9cca90cd392b7d1c
cchigo <ccasadu15@gmail.com>
2020-01-15 14:34:16 +0100

    create location data class

--- app/src/main/java/com/chigo/distancecalc/data/Location.kt
+++ app/src/main/java/com/chigo/distancecalc/data/Location.kt
@@ -1,4 +1,23 @@
 package com.chigo.distancecalc.data
 
-class Location {
+import android.os.Parcelable
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+import kotlinx.android.parcel.Parcelize
+
+@Entity(tableName = "location_table")
+@Parcelize
+data class Location (
+
+    @ColumnInfo(name = "longitude")
+    var longitude: Double,
+
+    @ColumnInfo(name = "latitude")
+    var latitude: Double
+
+): Parcelable
+{
+    @PrimaryKey(autoGenerate = true)
+    var id: Int = 0
 }
